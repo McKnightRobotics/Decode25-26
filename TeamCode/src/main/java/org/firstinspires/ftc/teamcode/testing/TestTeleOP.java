e6600cb1d96b157bb9d6ed78e869f76ebb330f18
EdgeRobotics <robotics20219@gmail.com>
2025-10-30 16:00:10 -0700

    Added all of the launcher class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestTeleOP.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.subsystems.GamepadEvents;
+import org.firstinspires.ftc.teamcode.subsystems.Launcher;
+
+@TeleOp(name="TestTeleOP")
+public class TestTeleOP extends LinearOpMode {
+
+    //init
+   Drive drive;
+   Launcher launcher;
+   GamepadEvents controls;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drive = new Drive(hardwareMap);
+        launcher = new Launcher();
+        controls = new GamepadEvents(gamepad1);
+        double forward;
+        double strafe;
+        double rotate;
+        waitForStart();
+        while (opModeIsActive()) {
+            forward = controls.left_stick_y;
+            strafe = controls.left_stick_x;
+            rotate = controls.right_stick_x;
+            drive.drive(forward, rotate, strafe);
+
+
+            // launcher button
+            if (gamepad1.a) {
+                launcher.startLauncher();
+            }
+
+
+            launcher.updateState();
+            controls.update();
+        }
+    }
+}
